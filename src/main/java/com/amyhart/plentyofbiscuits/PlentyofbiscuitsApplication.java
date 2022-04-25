3ef2bff5e51403bb36fe7276200a92002b341ab9
amy-hart <amy.hart@autotrader.co.uk>
2022-04-25 10:53:58 +0100

    Innitial Commit

---
+++ src/main/java/com/amyhart/plentyofbiscuits/PlentyofbiscuitsApplication.java
@@ -0,0 +1,13 @@
+package com.amyhart.plentyofbiscuits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlentyofbiscuitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlentyofbiscuitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amyhart/plentyofbiscuits/PlentyofbiscuitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amyhart.plentyofbiscuits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlentyofbiscuitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
